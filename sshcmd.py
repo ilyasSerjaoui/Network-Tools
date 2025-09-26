4fcfd4b9e2dde2cb61c8f5140f90f14782b3924a
ilyas serjaoui <100000265+ilyasSerjaoui@users.noreply.github.com>
2025-09-26 16:33:31 -0400

    Add files via upload

---
+++ sshcmd.py
@@ -0,0 +1,18 @@
+import threading
+import paramiko
+import subprocess
+
+def ssh_command(ip,user,command):
+    client = paramiko.SSHClient()
+    
+    # client.load_host_key('/home/justin/.ssh/known_hosts')
+    
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip,username=user,password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ss_session.recv(1024))
+    return
+    
+ssh_command("192.168.100.131",'username','password','id')
